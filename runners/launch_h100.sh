ca90eba7faf08a9f1ca06b8709f03b9179f28247
SURESH NAMPALLI <snampalli@habana.ai>
2025-11-11 23:36:44 +0200

    updated bash typo:

--- runners/launch_h100.sh
+++ runners/launch_h100.sh
@@ -56,6 +56,8 @@ fi
 # ============================================================
 git clone https://github.com/kimbochen/bench_serving.git
 
+NUM_PROMPTS=$(( CONC * 10 ))
+
 set -x
 docker run --rm --network=host --name=$client_name \
   -v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
@@ -72,7 +74,7 @@ docker run --rm --network=host --name=$client_name \
        --dataset-name=random \
        --random-input-len=\$ISL --random-output-len=\$OSL \
        --random-range-ratio=\$RANDOM_RANGE_RATIO \
-       --num-prompts=\$(( \$CONC * 10 )) --max-concurrency=\$CONC \
+       --num-prompts=$NUM_PROMPTS --max-concurrency=\$CONC \
        --request-rate=inf --ignore-eos \
        --save-result --percentile-metrics='ttft,tpot,itl,e2el' \
        --result-dir=/workspace/ \
